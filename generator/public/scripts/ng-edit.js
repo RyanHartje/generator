d79afabf6ec3d7c0a6a2ee9aade8b4fbf4ddc98b
Ryan Hartje <ryan@ryanhartje.com>
2014-10-20 21:32:53 -0500

    gogo

---
+++ generator/public/scripts/ng-edit.js
@@ -0,0 +1,4 @@
+angular.module("root", [])
+  .controller("index", ["$scope", function($scope){
+    $scope.navbar="<div class='navbar-header'><button type='button' data-toggle='collapse' data-target='.navbar-collapse' class='navbar-toggle'><span class='icon-bar'></span><span class='icon-bar'></span><span class='icon-bar'></span></button><a href='#' class='navbar-brand'>Generator </a></div><div class='collapse navbar-collapse'><ul class='nav navbar-nav'><li class='active'><a href='#'>Home</a></li><li><a href='#about'>About</a></li><li><a href='#contact'>Contact</a></li><li class='ui'><a id='save' href='#' class='ui'>Save</a></li><li class='ui'><a href='#' class='ui add_jumbotron'>Jumbo</a></li><li class='ui'><a href='#' class='ui add_bgblock'>BGBlock</a></li></ul><ul class='nav nav-bar navbar-right'><li class='editor'><a class='editor'><i class='i.glyphicon.glyphicon-pencil.editor'></i></a></li></ul></div></div></div>";
+  }]);
