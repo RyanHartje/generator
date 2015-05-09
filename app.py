6f9bbf667cdb8f6288eba5be829f230ea8adcd7a
Ryan Hartje <ryan@ryanhartje.com>
2015-05-08 21:20:54 -0500

    Header examples

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
 from flask import Flask, render_template
 from flask.ext.bootstrap import Bootstrap
+import random
 
 app = Flask(__name__)
 Bootstrap(app)
 
 @app.route('/')
 def index():
-  return render_template('index.html')
+  header = ['header0', 'header1', 'header2'] 
+  return render_template('index.html',header=header,die=random.randrange(0,len(header)))
+
 
 if __name__ == "__main__":
   app.run(debug=True,port=5000,host="0.0.0.0")
